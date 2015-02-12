8e0204b1f8769e86f2ae178d99d4b3dc297aa1e0
tony <anthonyjschulte@gmail.com>
2015-02-12 11:50:24 -0700

    now has 2 warmers

--- src/main/cpp/consoleWarmer.c
+++ src/main/cpp/consoleWarmer.c
@@ -9,9 +9,9 @@ int main()
   time_t t;
   srand((unsigned) time(&t));
 
-  int n = 60;
-  int n2 = 20;
-  int thisn = 0;
+  int n = 40;
+  int n2 = 41;
+
   while (1==1) {
     int nmod = rand() % 2;
 
@@ -25,12 +25,45 @@ int main()
     if(n > 80){
       n = 80;
     }
-  
-  
-    for (int i = 0 ;i < n; i++){
+    nmod = rand() % 2;
+
+    if(nmod == 0) {
+      nmod = -1;
+    }
+    n2 = n2 + nmod;
+    if(n2 < 0) {
+      n2 = 0;
+    }
+    if(n2 > 80){
+      n2 = 80;
+    }
+
+    int nl = n;
+    int nr = n2;
+    if(n > n2){
+      nl = n2;
+      nr = n;
+    }
+
+    for (int i = 0 ;i < nl; i++){
       printf(" ");
     }
-    printf("█\n");
+    if(n <= n2){
+      printf("█");
+    }
+    else{
+     printf("░");
+    }
+
+    for (int i = nl ;i < nr; i++){
+      printf(" ");
+    }
+    if(n >= n2){
+      printf("█\n");
+    }
+    else{
+     printf("░\n");
+    }
 
     usleep(500000);
   }
