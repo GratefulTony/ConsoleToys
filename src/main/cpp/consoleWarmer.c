cbcf954d3ac81fc201ae93e5311ad68b652f8cc4
GratefulTony <anthonyjschulte@gmail.com>
2015-02-12 11:11:23 -0700

    Create consoleWarmer.c

---
+++ src/main/cpp/consoleWarmer.c
@@ -0,0 +1,30 @@
+#include <ctime>
+#include <cstdlib>
+#include <unistd.h>
+
+srand(time(0));
+
+int n = 60;
+int n2 = 20;
+int thisn = 0;
+while (true) {
+  int nmod = rand() % 2;
+  if(nmod == 0) {
+    nmod = -1;
+  }
+  n = n + nmod;
+  if(n < 0) {
+    n = 0;
+  }
+  if(n > 80){
+    n = 80;
+  }
+  
+  
+  for (i = 0 ;n < 80; n++){
+    printf(" ");
+  }
+  printf("█\n");
+
+  usleep(500000);
+}
