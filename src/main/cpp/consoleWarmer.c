84e8ab7b825505c3290e51353e633ae41b7e973f
tony <anthonyjschulte@gmail.com>
2015-02-12 11:35:07 -0700

    it works, sort of.

--- src/main/cpp/consoleWarmer.c
+++ src/main/cpp/consoleWarmer.c
@@ -1,30 +1,37 @@
-#include <time.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<unistd.h>
 
-srand(5);
 
-int n = 60;
-int n2 = 20;
-int thisn = 0;
-while (true) {
-  int nmod = rand() % 2;
-  if(nmod == 0) {
-    nmod = -1;
-  }
-  n = n + nmod;
-  if(n < 0) {
-    n = 0;
-  }
-  if(n > 80){
-    n = 80;
-  }
+int main()
+{
+  time_t t;
+  srand((unsigned) time(&t));
+
+  int n = 60;
+  int n2 = 20;
+  int thisn = 0;
+  while (1==1) {
+    int nmod = rand() % 2;
+
+    if(nmod == 0) {
+      nmod = -1;
+    }
+    n = n + nmod;
+    if(n < 0) {
+      n = 0;
+    }
+    if(n > 80){
+      n = 80;
+    }
   
   
-  for (i = 0 ;n < 80; n++){
-    printf(" ");
-  }
-  printf("█\n");
+    for (int i = 0 ;i < n; i++){
+      printf(" ");
+    }
+    printf("█\n");
 
-  usleep(500000);
+    usleep(500000);
+  }
 }
